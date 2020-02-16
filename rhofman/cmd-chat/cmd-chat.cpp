2e6513611226107a0d5497f54b27cdda06914d7d
rhofman <hoffroma24@gmail.com>
2020-02-16 19:44:12 +0200

    added sender base code

--- rhofman/cmd-chat/cmd-chat.cpp
+++ rhofman/cmd-chat/cmd-chat.cpp
@@ -1,20 +1,58 @@
-// cmd-chat.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
+#include "winsock2.h"
 #include <iostream>
+
+#pragma comment(lib, "Ws2_32.lib")
+
+#define PORT 27015
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    WSADATA wsaData;
+    int error = WSAStartup(MAKEWORD(2, 2), &wsaData);
+    if (error != 0)
+    {
+        std::cout << "Networking lib cannot be initialized\n";
+        std::cout << "Error code: " << error << '\n';
+        return -1;
+    }
+
+    SOCKET sock = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sock == INVALID_SOCKET)
+    {
+        std::cout << "Unable to create socket\n";
+        std::cout << "Error code: " << WSAGetLastError() << '\n';
+        return -1;
+    }
+
+    char broadcast = '1';
+
+    if (setsockopt(sock, SOL_SOCKET, SO_BROADCAST, &broadcast, sizeof(broadcast)) == SOCKET_ERROR)
+    {
+        std::cout << "Unable to set socket parameters\n";
+        std::cout << "Error code: " << WSAGetLastError() << '\n';
+        closesocket(sock);
+        return -1;
+    }
+
+    int len = sizeof(struct sockaddr_in);
+
+    sockaddr_in addressReceiver;
+    addressReceiver.sin_family = AF_INET;
+    addressReceiver.sin_port = htons(PORT);
+    addressReceiver.sin_addr.s_addr  = INADDR_BROADCAST;
+
+    char message[] = "Broadcast message from SLAVE TAG";
+    sendto(sock, message, strlen(message) + 1, 0, (sockaddr*)& addressReceiver, sizeof(addressReceiver));
+
+    char buffer[50] = "";
+    int bufferSize = 50;
+    recvfrom(sock, buffer, bufferSize, 0, (sockaddr*)& addressReceiver, &len);
+
+    std::cout << "\n\n\tReceived message from Reader is =>  " << buffer;
+
+    std::cout << "\n\n\tpress any key to CONT...";
+    getchar();
+
+    closesocket(sock);
+    WSACleanup();
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
